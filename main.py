85f95193b2ad5e57f092d949ef26559a75d1ca16
Freezeey <huschdaniel@gmail.com>
2025-03-29 09:26:32 +0100

    added vars

---
+++ main.py
@@ -0,0 +1,11 @@
+str_pw_length = input("Enter the length of the password: ")
+
+nums = "0123456789"
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+special = "!@#$%^&*()-_=+[]{}|;:,.<>?/"
+
+all_characters = nums + lowercase + uppercase + special
+
+
+    
